510c2d3a288b2608389547136d175d5dde4e2ed8
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2021-04-23 02:01:16 -0500

    la chida
    la chida

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from configparser import MissingSectionHeaderError
+from ctypes import Array
+from logging import setLogRecordFactory
+from re import X
+from typing import NoReturn
+from kivy import app
+
+from kivymd.uix import screen
+from uix.list import OneLineListItem
 import kivy 
 from kivymd.app import MDApp
 from kivy.lang import Builder, builder
@@ -6,101 +14,45 @@ from kivymd.uix.screen import Screen
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivy.uix.widget import Widget
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.imagelist import SmartTileWithLabel
+from kivymd.uix.label import MDLabel
+from kivy.uix.widget import Widget
 
 
-screen_helper= """ 
-ScreenManager 
-    MenuScreen: 
+class mainmenu(Screen):
+    def hi (self,array=[]):
+        for x in range(100):
+            self.root.ids.list.add_widget(OneLineListItem(text = str(x)))
+            
     
+    pass
+
+class MyApp(MDApp):
     
-
-<MenuScreen>
-    name: 'menu'
-
-    BoxLayout:
-        orientation:'vertical'
+    def build(self):
+        print(dir(app))
+        #app.root.ids.grid.add_widget(MDLabel(text = "=asd"))
+        #app.Widget.root.ids.list.add_widget(MDLabel(text = "=asd"))
+        x = []
+        
+        mainmenu.hi(self,x)
 
         
-        MDToolbar:
-            title: 'APP'
-            md_bg_color: .1, .1, .1, 1
-            specific_text_color: 1, 1, 1, 1
-
-        MDBottomNavigation:
-            panel_color: 1, 1, 1, 1
-
-            MDBottomNavigationItem:
-
-                name: 'screen 1'
-                text: 'Calendario'
-                icon: 'calendar'
-                
-                ScrollView:
-                    MDList:
-
-                        OneLineListItem:
-                            text: "1"
-                        OneLineListItem:
-                            text: "2"
-                        OneLineListItem:
-                            text: "3"
-                        OneLineListItem:
-                            text: "4"
-                        OneLineListItem:
-                            text: "5"
-                        OneLineListItem:
-                            text: "6"
-                        OneLineListItem:
-                            text: "7"
-                        OneLineListItem:
-                            text: "8"
-                            
-                
-                
-                
-            MDBottomNavigationItem:
-                name: 'screen 2'
-                text: 'registrar'
-                icon: 'database-plus'
-                
-                height: "240dp"
-                ScrollView:
-                    MDGridLayout:
-                        cols: 4
-                        adaptive_height: True
-                        padding: dp(20), dp(20)
-                        spacing: dp(20)
-                        SmartTileWithLabel:
-                            source: "main-header.jpg"
-                            text: "hola"
-                            size_hint_y: None
-                            height: "120dp"
-                        SmartTileWithLabel:
-                            source: "main-header.jpg"
-                            text: "hola"
-                            size_hint_y: None
-                            height: "120dp"
-                        SmartTileWithLabel:
-                            source: "main-header.jpg"
-                            text: "hola"
-                            size_hint_y: None
-                            height: "120dp"
-                        SmartTileWithLabel:
-                            source: "main-header.jpg"
-                            text: "hola"
-                            size_hint_y: None
-                            height: "120dp"
-
-                        
     
-"""
-class MenuScreen(Screen):
-    pass
-class MyApp(MDApp):
 
-    def build(self):
-        screen = Builder.load_string(screen_helper)
-        return screen
+    
+   
+
+    
+    
+
+        return super().build()
+
+        
+
+        
 
 if __name__ == '__main__':
     MyApp().run()
